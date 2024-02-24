16429fc5b034e1cc90488219621d558f1f50b33c
TosLICK <anatosliy@gmail.com>
2024-02-24 15:13:15 +0200

    Added comments

--- get_numbers_ticket.py
+++ get_numbers_ticket.py
@@ -1,17 +1,17 @@
 import random
 
 def get_numbers_ticket(min: int, max: int, quantity: int) -> list:
-    if isinstance(min, int) and isinstance(max, int) and isinstance(quantity, int):
-        if min >= 1 and max <= 1000 and min < max:
-            numbers = []
+    if isinstance(min, int) and isinstance(max, int) and isinstance(quantity, int): # чи усі атрибути типу int
+        if min >= 1 and max <= 1000 and min < max: # чи задовольняють атрибути числовим обмеженням
+            numbers = [] 
             while quantity > 0:
-                number = random.randint(min, max)
-                if number in numbers:
+                number = random.randint(min, max) # генерується випадкове число у межах min і max
+                if number in numbers: # якщо згенероване число вже у списку, тоді ігноруємо і генеруємо нове число
                     continue
                 else:
-                    numbers.append(number)
+                    numbers.append(number) # якщо згенероване число відсутнє у списку, то воно додається до списку
                     quantity -= 1
-            return sorted(numbers)
+            return sorted(numbers) # сортуємо отриманий список
         else:
             return []
     else:
@@ -19,10 +19,10 @@ def get_numbers_ticket(min: int, max: int, quantity: int) -> list:
 
 # Ще варіант зробити список від min до max і використати random.sample(list, quantity): 
 # def get_numbers_ticket(min: int, max: int, quantity: int) -> list:
-#     if isinstance(min, int) and isinstance(max, int) and isinstance(quantity, int):
-#         if min >= 1 and max <= 1000 and min < max:
-#             numbers = [x for x in range(max+1)]
-#             return sorted(random.sample(numbers, quantity))
+#     if isinstance(min, int) and isinstance(max, int) and isinstance(quantity, int): # чи усі атрибути типу int
+#         if min >= 1 and max <= 1000 and min < max: # чи задовольняють атрибути числовим обмеженням
+#             numbers = [x for x in range(max+1)] # створюється список від min до max включно
+#             return sorted(random.sample(numbers, quantity)) # повертаємо відсортований список з отриманих чисел
 #         else:
 #             return []
 #     else:
